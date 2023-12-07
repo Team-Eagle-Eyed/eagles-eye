5d19790985858397b6334a86c07f9c5281db8791
John Gooden <johnnyg3587@gmail.com>
2023-12-06 21:42:44 -0600

    removed copyright from old template

--- events/ready.js
+++ events/ready.js
@@ -3,8 +3,6 @@ module.exports = {
 	once: true, // remove if false
 	async execute(client) { // stuff to do
 		console.info(`Ready at: ${client.readyAt}`);
-		console.info('Harold Bot Copyright (C) 2021-Present John Gooden');
-		console.info('Copyright info: https://github.com/TheHaroldBot/Harold/blob/main/LICENSE\n\n');
 		client.user.setActivity('Go 8825!');
 	},
 };
